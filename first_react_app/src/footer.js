78ece0202db66f05b237a6e72c9011befa09f77f
qinyuhua <2424709084@qq.com>
2018-01-03 10:05:02 +0800

    footer

---
+++ first_react_app/src/footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+class Footer extends Component {
+
+    render() {
+        return (
+            <footer className="footer">
+                <div className="footer_banner">交费</div>
+                <div className="footer_banner">代交</div>
+                <div className="footer_banner">发现</div>
+                <div className="footer_banner">我的</div>
+            </footer>
+        );
+    }
+}
+export default Footer;
--- first_react_app/src/index.js
+++ first_react_app/src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import ClickCounter from './ClickCounter';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(
+//     <App />,
+//     document.getElementById('root')
+// );
+ReactDOM.render(
+    <ClickCounter/>,
+    document.getElementById('root')
+);
 registerServiceWorker();
+// ClickCounter();
